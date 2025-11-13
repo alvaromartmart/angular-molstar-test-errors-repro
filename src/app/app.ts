2e418659dc7d53a515c8de064ce087e210071b5f
Álvaro Martínez <alvaro.mart.mart+dev@gmail.com>
2025-11-13 12:37:34 +0100

    feat: molstar test repro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { AfterViewInit, Component, DOCUMENT, ElementRef, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Viewer } from 'molstar/lib/apps/viewer/app';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements AfterViewInit {
   protected readonly title = signal('angular-molstar-repro');
+  protected v!: Viewer;
+  @ViewChild('molstarViewer', { static: true, read: ElementRef }) protected element!: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit() {
+    void this.initMolstarViewer();
+  }
+
+  protected async initMolstarViewer() {
+    this.v = await Viewer.create(this.element!.nativeElement);
+  }
 }
